eb0b774cfe3932a8b9b103f8f9c103764dba5ee5
Bastien <bastien.guillaumat@gmail.com>
2020-08-18 10:38:59 +0200

    Update the parsed data

--- README.md
+++ README.md
@@ -23,12 +23,20 @@ wavParser(base64file: string): any
 ```
 
 The result object will contain different values:
-```javascript
-fileSize: The size of the sound file
-formatType: Type of format (1 is PCM)
-channels: The number of channels
-sampleRate: The sample rate (eg: 8000kHz)
-bitsPerSample: The number of bits (eg: 16)
+```
+ChunkID: Contains the letters "RIFF" in ASCII form
+ChunkSize: This is the size of the rest of the chunk following this number. This is the size of the entire file in bytes minus 8 bytes for the two fields not included in this count ChunkID and ChunkSize.
+Format: Contains the letters "WAVE"
+SubChunk1ID: Contains the letters "fmt "
+SubChunk1Size: 16 for PCM. This is the size of the rest of the Subchunk which follows this number
+AudioFormat: 1 is PCM. Values other than 1 indicate some form of compression
+NumChannels: Mono = 1, Stereo = 2, etc 
+SampleRate: The sample rate (e.g: 8000kHz, 44100kHz, etc)
+ByteRate: SampleRate * NumChannels * BitsPerSample/8
+BlockAlign: (BitsPerSample * Channels) / 8 | 1 - 8 bit mono, 2 – 8 bit stereo/16 bit mono, 4 – 16 bit stereo
+BitsPerSample: The number of bits (e.g: 16 = 16 bits)
+SubChunk2ID: Contains the letters "data"
+SubChunk2Size: NumSamples * NumChannels * BitsPerSample/8. This is the number of bytes in the data (After this header)
 ```
 
 ## Usage ##
@@ -38,7 +46,7 @@ let reader = new FileReader();
 reader.onload = (ev) => {
     if (ev.target) {
         let waveFile = wavParser(ev.target.result);
-        console.log(waveFile.sampleRate);
+        console.log(waveFile.SampleRate);
     }
 };
 reader.readAsDataURL(file);
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,77 @@
 export function wavParser(base64sound: string): any {
-    let byteString = atob(base64sound.split(',')[1]);
+    let byteString = "";
+
+    if (base64sound.indexOf("audio/wav")) {
+        byteString = atob(base64sound.split(',')[1]);
+    } else {
+        byteString = atob(base64sound);
+    }
+
+    let ChunkID = byteString.charAt(0) + byteString.charAt(1) + byteString.charAt(2) + byteString.charAt(3);
+
+    let Format = byteString.charAt(8) + byteString.charAt(9) + byteString.charAt(10) + byteString.charAt(11);
+
+    let SubChunk1ID = byteString.charAt(12) + byteString.charAt(13) + byteString.charAt(14) + byteString.charAt(15);
+
+    if (ChunkID != "RIFF" || Format != "WAVE" || SubChunk1ID != "fmt ") {
+        console.error("Wrong type of file!");
+        return ;
+    }
+
     let resultObject = {} as any;
 
-    let fileSizeTMP = byteString.charCodeAt(7).toString(16)
+    resultObject.ChunkID = ChunkID;
+
+    let fileSize = byteString.charCodeAt(7).toString(16)
         + byteString.charCodeAt(6).toString(16)
         + byteString.charCodeAt(5).toString(16)
         + byteString.charCodeAt(4).toString(16);
-    resultObject.fileSize = parseInt(fileSizeTMP, 16);
+    resultObject.ChunkSize = parseInt(fileSize, 16);
+
+    resultObject.Format = Format;
+    resultObject.SubChunk1ID = SubChunk1ID;
 
     let length = byteString.charCodeAt(19).toString(16)
         + byteString.charCodeAt(18).toString(16)
         + byteString.charCodeAt(17).toString(16)
         + byteString.charCodeAt(16).toString(16);
-    //resultObject.lengthOfFormatData = parseInt(length, 16);
+    resultObject.SubChunk1Size = parseInt(length, 16);
 
-    let formatTypeTMP = byteString.charCodeAt(21).toString(16)
+    let formatType = byteString.charCodeAt(21).toString(16)
         + byteString.charCodeAt(20).toString(16);
-    resultObject.formatType = parseInt(formatTypeTMP, 16);
+    resultObject.AudioFormat = parseInt(formatType, 16);
 
-    let channelsTMP = byteString.charCodeAt(23).toString(16)
+    let channels = byteString.charCodeAt(23).toString(16)
         + byteString.charCodeAt(22).toString(16);
-    resultObject.channels = parseInt(channelsTMP, 16);
+    resultObject.NumChannels = parseInt(channels, 16);
 
-    let sampleRateTMP = byteString.charCodeAt(27).toString(16)
+    let sampleRate = byteString.charCodeAt(27).toString(16)
         + byteString.charCodeAt(26).toString(16)
         + byteString.charCodeAt(25).toString(16)
         + byteString.charCodeAt(24).toString(16);
-    resultObject.sampleRate = parseInt(sampleRateTMP, 16);
+    resultObject.SampleRate = parseInt(sampleRate, 16);
 
-    let bitsPerSampleTMP = byteString.charCodeAt(35).toString(16)
+    let byteRate = byteString.charCodeAt(31).toString(16)
+        + byteString.charCodeAt(30).toString(16)
+        + byteString.charCodeAt(29).toString(16)
+        + byteString.charCodeAt(28).toString(16);
+    resultObject.ByteRate = parseInt(byteRate, 16);
+
+    let BlockAlign = byteString.charCodeAt(33).toString(16)
+        + byteString.charCodeAt(32).toString(16);
+    resultObject.BlockAlign = parseInt(BlockAlign, 16);
+
+    let bitsPerSample = byteString.charCodeAt(35).toString(16)
         + byteString.charCodeAt(34).toString(16);
-    resultObject.bitsPerSample = parseInt(bitsPerSampleTMP, 16);
+    resultObject.BitsPerSample = parseInt(bitsPerSample, 16);
 
-    let sizeOfDataTMP = byteString.charCodeAt(40).toString(16)
-        + byteString.charCodeAt(41).toString(16)
+    resultObject.SubChunk2ID = byteString.charAt(36) + byteString.charAt(37) + byteString.charAt(38) + byteString.charAt(39);
+
+    let sizeOfData = byteString.charCodeAt(43).toString(16)
         + byteString.charCodeAt(42).toString(16)
-        + byteString.charCodeAt(43).toString(16);
-    //resultObject.sizeOfData = parseInt(sizeOfDataTMP, 16);
+        + byteString.charCodeAt(41).toString(16)
+        + byteString.charCodeAt(40).toString(16);
+    resultObject.SubChunk2Size = parseInt(sizeOfData, 16);
 
     return resultObject;
 }
